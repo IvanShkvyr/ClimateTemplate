914c9c9182c1b3446745713477780418d2aa3e1b
IvanShkvyr <106991993+IvanShkvyr@users.noreply.github.com>
2026-07-24 13:51:04 +0200

    fix: trim white nodata border left by CRS reprojection (#20)

--- clim4cast_imagegen/io/image_io.py
+++ clim4cast_imagegen/io/image_io.py
@@ -11,3 +11,21 @@ def open_rgba(path: Path) -> Image.Image:
 def save_image(image: Image, dst_path: Path, **kwargs) -> None:
     """Save an image to file"""
     image.save(dst_path, **kwargs)
+
+
+def trim_image_sides(
+        image_path: Path,
+        left: int = 0,
+        right: int = 0,
+        top: int = 0,
+        bottom: int = 0,
+    ) -> None:
+    """
+    Crop a fixed number of pixels from each side of an image, in place.
+    """
+    with Image.open(image_path) as img:
+        img.load()
+        width, height = img.size
+        cropped = img.crop((left, top, width - right, height - bottom))
+
+    cropped.save(image_path)
